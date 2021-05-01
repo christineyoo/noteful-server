b1e4adecb4292799f436a2135eee001dfc427975
Christine Yoo <christinecho.s@gmail.com>
2021-05-01 11:52:58 -0700

    added seed files

---
+++ seeds/seed.folders.sql
@@ -0,0 +1,5 @@
+INSERT INTO folders (folder_name)
+VALUES ('Home improvement'),
+    ('Coding Bootcamp'),
+    ('Work'),
+    ('Misc');
---
+++ seeds/seed.notes.sql
@@ -0,0 +1,8 @@
+INSERT INTO notes (note_name, content, folderId)
+VALUES
+    ('Bathroom', 'Install the shelves, clean the toilet and shower', 1),
+    ('Checkpoint 19', 'Finish the server side for Noteful app', 2),
+    ('Grading', 'Grade assignments from week 15, respond to emails', 3),
+    ('Lesson planning', 'Create lesson plans for week 16, figure out how to administer the SBAC', 3),
+    ('Books', 'Organize books, create a record of current titles', 1),
+    ('Buy containers', 'Go to Daiso to buy containers and soap dishes', 4);
